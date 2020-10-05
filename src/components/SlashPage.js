e4e22c11e9b69b19a043c3ca75e7220f7ef562fa
Nykarri <karri.bonez@gmail.com>
2020-10-05 10:38:52 -0400

    8th time

--- src/components/SlashPage.js
+++ src/components/SlashPage.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import logo from '../yd2hlanding2.gif'
 
 export default function SplashPage(props) {
     return (
         <>
         
         <div id="bg">
-          <img id="logo-gif" alt="YD2H logo gif" src="https://imgur.com/0zEkm0X" />  
+          <img id="logo-gif" alt="YD2H logo gif" src={logo} />  
         </div>
         
         </>
