86d041360ca468db0aaa8e0907f4955c441cab03
Nykarri <karri.bonez@gmail.com>
2020-10-06 14:49:02 -0400

    added video file

--- src/components/SlashPage.js
+++ src/components/SlashPage.js
@@ -1,17 +1,14 @@
 import React from "react";
-// import logo from '../yd2hlanding2.gif'
-
-
+import logo from '../yd2hlanding2.gif'
 
 export default function SplashPage(props) {
-  return (
-    <>
-
-      <div id="bg">
-        <img id="logo-gif" alt="YD2H logo gif" src="https://i.imgur.com/0zEkm0X.gifv" />  
-        {/* <iframe class="imgur-embed" width="100%" height="1080" frameborder="0" src="https://i.imgur.com/0zEkm0X.gifv#embed"></iframe> */}
-      </div>
-
-    </>
-  )
+    return (
+        <>
+        
+        <div id="bg">
+          <img id="logo-gif" alt="YD2H logo gif" src={logo} />  
+        </div>
+        
+        </>
+    )
 }
