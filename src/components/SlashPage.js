daf8b5a31abb9adfb479cc0a7242f980066ce42d
Nykarri <karri.bonez@gmail.com>
2020-10-06 16:16:43 -0400

    fixed deployment

--- src/components/SlashPage.js
+++ src/components/SlashPage.js
@@ -7,7 +7,7 @@ export default function SplashPage(props) {
   return (
     <>
     <div id="bg">
-      <iframe src="https://player.vimeo.com/video/465498752?autoplay=1&loop=1&title=0&byline=0&portrait=0" width="2000" height="1000" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+      <iframe id="iframe" src="https://player.vimeo.com/video/465498752?autoplay=1&loop=1&title=0&byline=0&portrait=0" width="2000" height="1000" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
     </div>
     
       {/* <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/465498752?autoplay=1&loop=1" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe></div> */}
